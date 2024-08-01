5e4df061f943ba02c3ae95491472920410227d31
gjburke2 <gjburke2@illinois.edu>
2024-08-01 17:26:36 -0500

    Implemented getting the piece by id in PieceScreen, a bit inefficient though
    The PieceScreen currently has to get all pieces, unable to just select its info by id. Might want to make it a dictionary of IDs and piecetypes where you can just select instead of filtering through an array every time. That  but it should figure itself out. Maybe an ordered dictionary or something, or store an array and dictionary at the same time for the dual purposes.

--- app/screens/PieceScreen.tsx
+++ app/screens/PieceScreen.tsx
@@ -1,19 +1,25 @@
 import { StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
 import { RootStackParams } from '../navigators/MainNavigator';
+import { useAppSelector } from '../hooks';
 
 type Props = NativeStackScreenProps<RootStackParams, "PieceScreen">;
 
 export default function PieceScreen({ route, navigation }: Props) {
-    return (
-        <View style={ styles.container }>
-            <Text>This is the piece screen</Text>
-            <Text>The id for the piece is: { route.params.id }</Text>
-            <Pressable onPress={() => navigation.goBack()}>
-              <Text>GO BACK</Text>
-            </Pressable>
-        </View>
-    );
+  const pieces = useAppSelector((state) => state.clothes.pieces);
+  const ID = route.params.id;
+
+  const piece = pieces.filter((piece) => piece.id === ID)[0];
+  
+  return (
+    <View style={ styles.container }>
+      <Text>This is the piece screen</Text>
+      <Text>The name for the piece is: { piece.name }</Text>
+      <Pressable onPress={() => navigation.goBack()}>
+        <Text>GO BACK</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
