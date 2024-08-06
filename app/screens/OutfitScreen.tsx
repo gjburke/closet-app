6223d9f533f833232df577c150ff3b2e658c0ece
gjburke2 <gjburke2@illinois.edu>
2024-08-06 14:33:30 -0500

    Added OutfitList to OutfitScreen

--- app/screens/OutfitScreen.tsx
+++ app/screens/OutfitScreen.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import OutfitList from "../components/OutfitList";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>This is the outfits tab</Text>
+      <OutfitList/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+  },
+});
