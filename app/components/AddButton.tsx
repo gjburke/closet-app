850bd36675c70fa09fcd162deda71cbef5cba8bc
gjburke2 <gjburke2@illinois.edu>
2024-08-07 15:57:36 -0500

    Small change to AddButton's format

--- app/components/AddButton.tsx
+++ app/components/AddButton.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Pressable, Text } from 'react-native'
-import {Dimensions} from 'react-native';
+import { Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParams } from '../navigators/MainNavigator';
 import type { StackNavigationProp } from '@react-navigation/stack';  
