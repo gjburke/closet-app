b56b979f04b7ea79889497790a1188adf11ae9ba
gjburke2 <gjburke2@illinois.edu>
2024-08-14 16:38:58 -0500

    Added TextInput for editting the name

--- app/components/closet/clothes/PieceEditor.tsx
+++ app/components/closet/clothes/PieceEditor.tsx
@@ -1,21 +1,25 @@
 import { Image, StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
 import { ItemProps } from '../../../screens/PieceScreen';
+import { useState } from 'react';
 
 export default function PieceEditor({ piece }: ItemProps) {
-  return (
-    <View style={ styles.container }>
-      <Text>This is the piece editor</Text>
-      <Text>The name for the piece is: { piece.name }</Text>
-      <Image source={ (() => {
-        if (!piece.image_uri) {
-          return require('./../../../../assets/splash.png');
-        } else {
-          return { uri: piece.image_uri };
-        }
-      })()
-    } style={ styles.image }/>
-    </View>
-  );
+    const [name, setName] = useState(piece.name)
+
+    return (
+        <View style={ styles.container }>
+        <Text>This is the piece editor</Text>
+        <Text>Update Name: </Text>
+        <TextInput style={{ height: 40 }} onChangeText={newName => setName(newName)} defaultValue={name}/>
+        <Image source={ (() => {
+            if (!piece.image_uri) {
+            return require('./../../../../assets/splash.png');
+            } else {
+            return { uri: piece.image_uri };
+            }
+        })()
+        } style={ styles.image }/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
