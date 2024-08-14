b194ca1b4a7bc0409c4da6d96bb07cf69d5eb4a6
gjburke2 <gjburke2@illinois.edu>
2024-08-14 17:42:41 -0500

    Implemented deletePiece in PieceScreen button

--- app/screens/PieceScreen.tsx
+++ app/screens/PieceScreen.tsx
@@ -5,6 +5,8 @@ import PieceView from '../components/closet/clothes/PieceView';
 import { PieceType } from './../components/closet/clothes/clothesSlice';
 import { useState } from 'react';
 import PieceEditor from '../components/closet/clothes/PieceEditor';
+import { useDispatch } from 'react-redux';
+import { deletePiece } from './../components/closet/clothes/clothesSlice'
 
 export type ItemProps = { piece: PieceType }
 type Props = NativeStackScreenProps<RootStackParams, "PieceScreen">;
@@ -12,6 +14,7 @@ type Props = NativeStackScreenProps<RootStackParams, "PieceScreen">;
 export default function PieceScreen({ route, navigation }: Props) {
   const piece = route.params.piece;
   const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useDispatch();
 
   function edit() {
     setIsEditing(true);
@@ -19,14 +22,15 @@ export default function PieceScreen({ route, navigation }: Props) {
   function revert() {
     setIsEditing(false);
   }
-  function deletePiece() {
+  function remove() {
+    dispatch(deletePiece(piece.name))
     navigation.goBack();
   }
   
   if (isEditing) {
     return (
       <View style= { styles.container }>
-        <Pressable onPress={deletePiece}>
+        <Pressable onPress={remove}>
           <Text>Delete Piece</Text>
         </Pressable>
         <Pressable onPress={revert}>
