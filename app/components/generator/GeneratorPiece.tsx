a33a5a7f1def352929c3f10c8f78851b985e9dc1
gjburke2 <gjburke2@illinois.edu>
2024-08-15 15:12:59 -0500

    Added delete button with dispatch to GeneratorPiece

--- app/components/generator/GeneratorPiece.tsx
+++ app/components/generator/GeneratorPiece.tsx
@@ -1,23 +1,34 @@
 import { Image, View, Pressable, StyleSheet, Text } from 'react-native';
 import { Dimensions } from 'react-native';
 import { ItemProps } from './GeneratorList';
+import { useAppDispatch } from '../../hooks';
+import { deletePiece } from './generatorSlice';
 
 export default function GeneratorPiece( { piece }: ItemProps) {
-    return (
-        <View>
-            <View style={styles.piece}>
-                <Text>This is a piece of clothing with name: {piece.name}!</Text>
-                <Image source={ (() => {
-                    if (!piece.image_uri) {
-                      return require('./../../../assets/splash.png');
-                    } else {
-                      return { uri: piece.image_uri };
-                    }
-                  })()
-                } style={ styles.image }/>
-            </View>
-        </View>
-    );
+  const dispatch = useAppDispatch();
+
+  function remove() {
+    dispatch(deletePiece(piece.name));
+  }
+
+  return (
+      <View>
+          <View style={styles.piece}>
+              <Text>This is a piece of clothing with name: {piece.name}!</Text>
+              <Image source={ (() => {
+                  if (!piece.image_uri) {
+                    return require('./../../../assets/splash.png');
+                  } else {
+                    return { uri: piece.image_uri };
+                  }
+                })()
+              } style={ styles.image }/>
+              <Pressable onPress={remove}>
+                <Text>Remove Piece</Text>
+              </Pressable>
+          </View>
+      </View>
+  );
 }
 
 const windowWidth = Dimensions.get('window').width;
