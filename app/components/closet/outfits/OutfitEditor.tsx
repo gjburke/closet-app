f99d5c6b1ded1cdb292d71aace156cb374baa34b
gjburke2 <gjburke2@illinois.edu>
2024-08-15 16:34:24 -0500

    Added GeneratorList to OutfitEditor, Save Changes Button, no logic yet

--- app/components/closet/outfits/OutfitEditor.tsx
+++ app/components/closet/outfits/OutfitEditor.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { ItemProps } from '../../../screens/OutfitScreen';
+import GeneratorList from '../../generator/GeneratorList';
 
 export default function OutfitEditor({ outfit }: ItemProps) {
     return (
         <View style={ styles.container }>
             <Text>This is the outfit editor</Text>
             <Text>The name for the outfit is: { outfit.name }</Text>
+            <GeneratorList/>
+            <Pressable>
+                <Text>Save Changes</Text>
+            </Pressable>
         </View>
     );
 }
