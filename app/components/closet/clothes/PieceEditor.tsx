c8cc78c140f966669dc961a3009f54c1f85fd3b6
gjburke2 <gjburke2@illinois.edu>
2024-08-19 16:00:58 -0500

    Added image retaking to PieceEditor, small bugs but will fix later

--- app/components/closet/clothes/PieceEditor.tsx
+++ app/components/closet/clothes/PieceEditor.tsx
@@ -3,45 +3,74 @@ import { ItemProps } from '../../../screens/PieceScreen';
 import { useState } from 'react';
 import { useAppDispatch } from '../../../hooks';
 import { PieceType, editPiece } from './clothesSlice';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function PieceEditor({ piece }: ItemProps) {
     const [name, setName] = useState(piece.name);
     const [type, setType] = useState(piece.type);
     const [size, setSize] = useState(piece.size);
     const [color, setColor] = useState(piece.color);
+    const [image, setImage] = useState(piece.image_uri);
     const dispatch = useAppDispatch();
 
     function saveEdits() {
         const newPiece: PieceType = {
-            ...piece,
             name: name,
             type: type,
             size: size,
             color: color,
+            image_uri: image,
         };
         alert('Edits Saved');
         dispatch(editPiece({ name: piece.name, newPiece }));
     }
 
+    // Issue with image not re-rendering once photo is taken, not sure why yet
+    // But it does still work technically, one you go out and in it changes correctly
+    async function takePhoto() {
+      const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+      if (permissionResult.granted == false) {
+        alert("You have not allowed camera permissions");
+        return;
+      }
+
+      let result = await ImagePicker.launchCameraAsync({
+        cameraType: ImagePicker.CameraType.back,
+        quality: 0,
+      });
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri)
+      }
+    }
+
     return (
         <View style={ styles.container }>
         <Text>This is the piece editor</Text>
-        <Image source={ (() => {
-            if (!piece.image_uri) {
-            return require('./../../../../assets/splash.png');
-            } else {
-            return { uri: piece.image_uri };
-            }
-        })()
-        } style={ styles.image }/>
-        <Text>Update Name: </Text>
-        <TextInput style={{ height: 30 }} onChangeText={newName => setName(newName)} defaultValue={name}/>
-        <Text>Update Type: </Text>
-        <TextInput style={{ height: 30 }} onChangeText={newType => setName(newType)} defaultValue={type}/>
-        <Text>Update Size: </Text>
-        <TextInput style={{ height: 30 }} onChangeText={newSize => setName(newSize)} defaultValue={size}/>
-        <Text>Update Color: </Text>
-        <TextInput style={{ height: 30 }} onChangeText={newColor => setName(newColor)} defaultValue={color}/>
+        <View style={ styles.imagePicker }>
+            <Image source={ (() => {
+                if (!piece.image_uri) {
+                return require('./../../../../assets/splash.png');
+                } else {
+                return { uri: image };
+                }
+            })()
+            } style={ styles.image }/>
+            <Pressable onPress={takePhoto}>
+            <Text>Take Photo</Text>
+            </Pressable>
+        </View>
+        <View>
+            <Text>Update Name: </Text>
+            <TextInput style={{ height: 30 }} onChangeText={newName => setName(newName)} defaultValue={name}/>
+            <Text>Update Type: </Text>
+            <TextInput style={{ height: 30 }} onChangeText={newType => setType(newType)} defaultValue={type}/>
+            <Text>Update Size: </Text>
+            <TextInput style={{ height: 30 }} onChangeText={newSize => setSize(newSize)} defaultValue={size}/>
+            <Text>Update Color: </Text>
+            <TextInput style={{ height: 30 }} onChangeText={newColor => setColor(newColor)} defaultValue={color}/>
+        </View>
         <Pressable onPress={saveEdits}>
             <Text>Save Edits</Text>
         </Pressable>
@@ -58,5 +87,10 @@ const styles = StyleSheet.create({
   image: {
       height: 200,
       width: 200,
+  },
+  imagePicker: {
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
