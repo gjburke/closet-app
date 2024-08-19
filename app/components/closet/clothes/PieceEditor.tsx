569bf6fff872209019ca1687848bcb024859568e
gjburke2 <gjburke2@illinois.edu>
2024-08-19 15:45:42 -0500

    Added rest of PieceType properties to PieceEditor (except image)

--- app/components/closet/clothes/PieceEditor.tsx
+++ app/components/closet/clothes/PieceEditor.tsx
@@ -6,12 +6,18 @@ import { PieceType, editPiece } from './clothesSlice';
 
 export default function PieceEditor({ piece }: ItemProps) {
     const [name, setName] = useState(piece.name);
+    const [type, setType] = useState(piece.type);
+    const [size, setSize] = useState(piece.size);
+    const [color, setColor] = useState(piece.color);
     const dispatch = useAppDispatch();
 
     function saveEdits() {
         const newPiece: PieceType = {
             ...piece,
             name: name,
+            type: type,
+            size: size,
+            color: color,
         };
         alert('Edits Saved');
         dispatch(editPiece({ name: piece.name, newPiece }));
@@ -20,8 +26,6 @@ export default function PieceEditor({ piece }: ItemProps) {
     return (
         <View style={ styles.container }>
         <Text>This is the piece editor</Text>
-        <Text>Update Name: </Text>
-        <TextInput style={{ height: 40 }} onChangeText={newName => setName(newName)} defaultValue={name}/>
         <Image source={ (() => {
             if (!piece.image_uri) {
             return require('./../../../../assets/splash.png');
@@ -30,6 +34,14 @@ export default function PieceEditor({ piece }: ItemProps) {
             }
         })()
         } style={ styles.image }/>
+        <Text>Update Name: </Text>
+        <TextInput style={{ height: 30 }} onChangeText={newName => setName(newName)} defaultValue={name}/>
+        <Text>Update Type: </Text>
+        <TextInput style={{ height: 30 }} onChangeText={newType => setName(newType)} defaultValue={type}/>
+        <Text>Update Size: </Text>
+        <TextInput style={{ height: 30 }} onChangeText={newSize => setName(newSize)} defaultValue={size}/>
+        <Text>Update Color: </Text>
+        <TextInput style={{ height: 30 }} onChangeText={newColor => setName(newColor)} defaultValue={color}/>
         <Pressable onPress={saveEdits}>
             <Text>Save Edits</Text>
         </Pressable>
