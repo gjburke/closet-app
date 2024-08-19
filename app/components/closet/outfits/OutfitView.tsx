c471bb50f03713c045e74808dddecfdfa00f458e
gjburke2 <gjburke2@illinois.edu>
2024-08-19 15:19:08 -0500

    Added PiecesList to the OutfitView

--- app/components/closet/outfits/OutfitView.tsx
+++ app/components/closet/outfits/OutfitView.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { ItemProps } from '../../../screens/OutfitScreen';
+import PiecesList from './PiecesList';
 
 export default function OutfitView({ outfit }: ItemProps) {
     return (
         <View style={ styles.container }>
             <Text>This is the outfit view</Text>
             <Text>The name for the outfit is: { outfit.name }</Text>
+            <PiecesList/>
         </View>
     );
 }
