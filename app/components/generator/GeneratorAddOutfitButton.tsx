6db18c4bf509737fed195290aaa8ec32084014ce
gjburke2 <gjburke2@illinois.edu>
2024-08-20 16:42:29 -0500

    Comment outlining issues

--- app/components/generator/GeneratorAddOutfitButton.tsx
+++ app/components/generator/GeneratorAddOutfitButton.tsx
@@ -4,6 +4,11 @@ import { Dimensions } from 'react-native';
 import { RootStackParams } from '../../navigators/MainNavigator';
 import type { StackNavigationProp } from '@react-navigation/stack';  
 
+// Ultimately the whole wayt that this will have to be done is going to be very different, likely involving passing props down and such
+// My idea is: have lists need to have props passed into them, then lists can be more universal
+// And this button will be able to know when things are added in (possibly?) so then stuff will update correctly
+// Basically, need to have the redux store calls above everything that needs to update
+// So for this and the GeneratorList, should get the revelant lists and stuff passed as props, and all the redux is help by higher stuff
 export default function GeneratorAddOutfitButton() {
     const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
 
